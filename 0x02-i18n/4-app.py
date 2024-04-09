4ce872673b8666d1e9a7fa5708a41a9da91b8c1d
Toyin Obafemi <byhisgraceabba@gmail.com>
2024-04-09 15:03:41 +0100

    Force locale with URL parameter

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+This is a Flask app with Babel extension configured and translations.
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """
+    Configuration class for Flask app.
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Determine the best-matching language for the user.
+    """
+    if 'locale' in request.args and request.args['locale'] in app.config['LANGUAGES']:
+        return request.args['locale']
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> str:
+    """
+    Render the index.html template with translated messages.
+    """
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
