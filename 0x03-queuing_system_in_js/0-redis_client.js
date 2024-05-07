7380a28bbc896c273fa02493095819737f38e149
Toyin Obafemi <byhisgraceabba@gmail.com>
2024-05-07 22:21:24 +0100

    Node Redis Client

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,14 @@
+const redis = require('redis');
+
+// Create a Redis client
+const client = redis.createClient();
+
+// Event listener for successful connection
+client.on('connect', () => {
+    console.log('Redis client connected to the server');
+});
+
+// Event listener for connection error
+client.on('error', (err) => {
+    console.error(`Redis client not connected to the server: ${err}`);
+});
